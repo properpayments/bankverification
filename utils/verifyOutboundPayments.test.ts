aa13ce0615a9b8471cb8a9e451f5a0f7448ab6c2
Christian Kastbjerg <christian@helloproper.com>
2021-06-24 11:46:22 +0200

    :white_check_mark: Update test for malformed payments

--- utils/verifyOutboundPayments.test.ts
+++ utils/verifyOutboundPayments.test.ts
@@ -18,8 +18,14 @@ describe("verifyOutboundPayments", () => {
   it("it crashes for malformed payments CSV file", () => {
     const mock = getMock("./mocks/payments_malformed.csv");
     const data = parseString(mock);
-    const t = () => verifyOutboundPayments(data, accounts);
-    expect(t).toThrow('Unexpected key "Tkst"');
+    const messages = verifyOutboundPayments(data, accounts);
+    expect(messages).toEqual([
+      {
+        id: "",
+        code: "invalid-file-format",
+        type: "error",
+      },
+    ]);
   });
   it("returns expected messages for unexpected data in the CSV", () => {
     const mock = getMock("./mocks/payments_with_errors.csv");
